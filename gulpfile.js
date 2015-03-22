c707e57c2395170cb2dbe80f6983d6f9b280cfd8
Kevin Isom <kevin.isom@gmail.com>
2015-03-22 20:37:18 +1300

    buid the css and use es6 for the react bits

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,60 @@
 var gulp = require('gulp'),
   browserify = require('browserify'),
   source = require('vinyl-source-stream'),
-  babelify = require('babelify');
+  babelify = require('babelify'),
+  rework = require('gulp-rework'),
+  inherit = require('rework-inherit'),
+  vars = require('rework-vars'),
+  imprt = require('rework-import'),
+  autoprefixer = require('gulp-autoprefixer'),
+  reworkNPM = require('rework-npm'),
+  plumber = require('gulp-plumber'),
+  watch = require('gulp-watch'),
+  media = require('rework-custom-media');
+ 
+gulp.task('watch', function() {
+    //var server = plugins.livereload();
 
+    watch('./admin/**/*.*' , function(file) {
+        gulp.start('buildjs');
+        //server.changed(file.path);
+    });
+    watch('./css/**/*.css', function(file) {
+        gulp.start('buildcss');
+        //server.changed(file.path);
+    });
+
+});
+gulp.task('buildcss', function () {
+    var mediaOptions = {
+      map: {
+        '--small-screen': 'screen and (max-width:40em)',
+        '--medium-screen': 'screen and (min-width: 40em)',
+        '--large-screen': 'screen and (min-width: 60em)'
+      }
+    };
+    return gulp.src('./css/style.css')
+        .pipe(rework(reworkNPM({ 
+            shim: { 
+                'purecss': 'build/pure.css',
+                'font-awesome' : 'css/font-awesome.css'
+            }}),
+            media(mediaOptions),
+            vars(), 
+            inherit(),
+            imprt({
+                path: './css/modules/'
+            })
+            )
+        )
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('templates/css/'));
+});
  
- 
-gulp.task('build', function () {
+gulp.task('buildjs', function () {
  
     return browserify({ entries:['./admin/app.js'], debug: true })
         .transform(babelify.configure({
@@ -27,5 +76,5 @@ gulp.task('build', function () {
         });
 });
  
- 
+ gulp.task('build', ['buildjs', 'buildcss']);
 gulp.task('default', [ 'build' ]);
