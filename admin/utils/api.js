bdd0a5a0c9419695995b098aec3852ee1947bbbb
Kevin Isom <kevin.isom@gmail.com>
2015-03-28 08:31:32 +1300

    call api end point to get settings

--- admin/utils/api.js
+++ admin/utils/api.js
@@ -2,19 +2,19 @@ import request from 'superagent';
 import ActionCreators from '../actions/action-creators';
 
 export default {
-    getAllPages() {
+    getAllPages: function () {
         request.get('/xms/api/pages')
             .end((err, res) => {
                 console.log('receivePages');
                 console.log(res);
                 ActionCreators.receivePages(res.body);
-            })
-    }
-    getAllSettings() {
+            });
+    },
+    getAllSettings: function () {
         request.get('/xms/api/settings')
             .end((err, res) => { 
                 console.log(res);
                 ActionCreators.receiveSettings(res.body);
-            })
+            });
     }
 }
