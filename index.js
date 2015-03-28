3c40d0619c406a0b2248ddf3056b04feba8c7683
Kevin Isom <kevin.isom@gmail.com>
2015-03-28 23:31:55 +1300

    tidy

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ var exposeData = function exposeXMSData (req, res, next) {
 var cms = require('./lib/cms');
 var loader = require('./lib/seed');
 exports.handle = cms.handler;
-
 exports.extend = function (app) {
-    loader();   
-    console.log(cms);
+    loader();
     //at this stage only one cms content route
     app.use(cms.handler);
-
     //get admin features and bolt them onto the app
-    var admin = require('./lib/admin')(app);
- 
+    require('./lib/admin')(app);
 };
