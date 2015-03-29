bcc803e93dd88c41481e3fb132fb7f33e2140d25
Kevin Isom <kevin.isom@gmail.com>
2015-03-29 15:18:28 +1300

    update date fields to be dates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var exposeData = function exposeXMSData (req, res, next) {
     next();
 };
@@ -5,7 +6,25 @@ var cms = require('./lib/cms');
 var loader = require('./lib/seed');
 exports.handle = cms.handler;
 exports.extend = function (app) {
-    loader();
+    loader(function () {
+        var pages = require('mongo-start')('pages');
+
+        pages.find(function (err, docs) {
+            for (var i = 0; i < docs.length; i++) {
+                if(docs[i].publishOn !== null){
+                    let po = new Date(docs[i].publishOn);
+                    docs[i].publishOn = po;
+                    let co = new Date(docs[i].createdOn);
+                    docs[i].createdOn = co;
+                    pages.save(docs[i]);
+                } else {
+                                        let co = new Date(docs[i].createdOn);
+                    docs[i].createdOn = co;
+                    pages.save(docs[i]);
+                }
+            };
+        })
+    });
     //at this stage only one cms content route
     app.use(cms.handler);
     //get admin features and bolt them onto the app
