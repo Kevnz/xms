1470d4038fff2c7ae556c69d247022c7e7fd567a
Kevin Isom <kevin.isom@gmail.com>
2015-03-31 07:23:16 +1300

    handle additional states of the pages

--- admin/pages-client.js
+++ admin/pages-client.js
@@ -1,11 +1,15 @@
-var agent = require('superagent');
-var PAGES_URI = '/xms/api/pages';
-var PAGE_URI = '/xms/api/pages/{id}';
-var format = require("string-template")
+import agent from 'superagent';
+import format from 'string-template';
 
-var defPages = [{"_id":"549394dd0e22880de0000001","view":"index","content":"This is a test","title":"Test Title","createdOn":1418956973794,"createdBy":"kev_nz"},{"_id":"549395360e22880de0000002","view":"index","content":"Another Test","title":"Another Test Title","createdOn":1418958114420,"createdBy":"kev_nz"},{"_id":"54939de1972a76e2e8000001","view":"index","content":"A third","title":"Third Test Title","createdOn":1418960039157,"createdBy":"kev_nz"}];
+const PAGES_URI = '/xms/api/pages';
+const PAGE_URI = '/xms/api/pages/{id}';
+const PUBLISHED_PAGES_URI = '/xms/api/pages/published';
+const DRAFT_PAGES_URI = '/xms/api/pages/draft';
+const QUEUED_PAGES_URI = '/xms/api/pages/queued';
+const DELETED_PAGES_URI = '/xms/api/pages/deleted';
 
 var PagesClient = {
+    
     
     getPages: function (callback) {
         agent
