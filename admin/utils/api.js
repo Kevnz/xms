508b112dfce607a67916469d1063d9ab72480847
Kevin Isom <kevin.isom@gmail.com>
2015-03-31 22:50:57 +1300

    API changes

--- admin/utils/api.js
+++ admin/utils/api.js
@@ -1,6 +1,13 @@
 import request from 'superagent';
 import ActionCreators from '../actions/action-creators';
 
+const PAGES_URI = '/xms/api/pages';
+const PAGE_URI = '/xms/api/pages/{id}';
+const PUBLISHED_PAGES_URI = '/xms/api/pages/published';
+const DRAFT_PAGES_URI = '/xms/api/pages/draft';
+const QUEUED_PAGES_URI = '/xms/api/pages/queued';
+const DELETED_PAGES_URI = '/xms/api/pages/deleted';
+
 export default {
     getAllPages: function () {
         request.get('/xms/api/pages')
@@ -10,6 +17,9 @@ export default {
                 ActionCreators.receivePages(res.body);
             });
     },
+    getPagesByStatus:function (status) {
+
+    }
     getAllSettings: function () {
         request.get('/xms/api/settings')
             .end((err, res) => { 
