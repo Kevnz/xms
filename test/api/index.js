dcabc3f7892b4d0495de178c910e80b9b33b0126
Kevin Isom <kevin.isom@gmail.com>
2015-04-01 23:29:05 +1300

    ES 6 that test

--- test/api/index.js
+++ test/api/index.js
@@ -7,16 +7,16 @@ var assert = require('assert');
 console.dir(xms);
 xms.extend(app);
 
-describe('The Admin HTTP API', function () {
-	describe('getting pages', function () {
-		describe('GET /pages/published', function () {
-			it('should return a list of pages', function (done) {
+describe('The Admin HTTP API', () => {
+	describe('getting pages',  () => {
+		describe('GET /pages/published',  () => {
+			it('should return a list of pages',  (done) => {
 				request(app)
 					.get('/xms/api/pages/published')
 					.expect('Content-Type', /json/)
 					.expect('Content-Length', '20')
 					.expect(200)
-					.end(function(err, res){
+					.end((err, res) => {
 						if (err) throw err;
 						console.log(res);
 						assert.ok(res.length > 0);
