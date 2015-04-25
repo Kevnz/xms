ced6b43bdd922253ee748edfd67decfb26c1f9fd
Kevin Isom <kevin.isom@gmail.com>
2015-04-25 12:17:03 +1200

    actions

--- admin/actions/action-creators.js
+++ admin/actions/action-creators.js
@@ -1,8 +1,25 @@
 import alt from '../alt';
 
-module.exports = alt.generateActions('receivePages',
-    'editPage',
-    'addPage',
-    'removePage',
-    'pageSelected',
-    'receiveSettings');
+import api from '../utils/api';
+
+class ActionsCreators {
+    constructor() {
+        this.generateActions(
+            'receivePages',
+            'editPage',
+            'addPage',
+            'removePage',
+            'pageSelected',
+            'receiveSettings'
+            );
+    }
+    pageUpdated(page) {
+        this.dispatch(page._id);
+        //api.updatePage(page);
+    }
+    settingSelected (setting) {
+        this.dispatch(setting.name.toLowerCase());
+        api.getPagesByStatus(setting.name.toLowerCase());
+    }
+}
+alt.createActions(ActionsCreators, exports);
