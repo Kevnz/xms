a46ba1b115eb49268bf52d2fcfa846edcdb229cf
Kevin Isom <kevin.isom@gmail.com>
2015-06-06 22:44:34 +1200

    Animations

--- admin/components/admin-app.js
+++ admin/components/admin-app.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react/addons';
 import Menu from './menu';
 import List from './list';
 import Main from './main';
 import PageStore from '../stores/page-store';
 import SettingsStore from '../stores/settings-store';
+
+const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 function _getStateFromStores () {
     console.log('_getStateFromStores');
@@ -27,12 +29,15 @@ export default class AdminApp extends React.Component {
         PageStore.unlisten(this._onChange.bind(this));
     }
     render() {
-        var selectedPage = this.state.pages.selectedPage ? this.state.pages.selectedPage._id : ''
+        var selectedPage = this.state.pages.selectedPage ? this.state.pages.selectedPage._id : {};
+        console.log('render the selected page from the main app', this.state.pages.selectedPage);
         return (
             <div className="app">
                 <Menu></Menu>
-                <List pages={this.state.pages.pages} selectedPage={selectedPage} ></List>
-                <Main page={this.state.pages.selectedPage} ></Main>
+                <List pages={this.state.pages.pages} selectedPage={this.state.pages.selectedPage} ></List>
+                <ReactCSSTransitionGroup transitionName="example">
+                    <Main page={this.state.pages.selectedPage} key={this.state.pages.selectedPage._id}></Main>
+                </ReactCSSTransitionGroup>
             </div>
         );
     }
