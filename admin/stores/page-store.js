3fcd40b8f3161372c70520467945ddcab997d02d
Kevin Isom <kevin.isom@gmail.com>
2015-06-06 22:39:27 +1200

    Handle more events

--- admin/stores/page-store.js
+++ admin/stores/page-store.js
@@ -5,6 +5,21 @@ class PageStore {
     constructor() {
         this.bindActions(ActionCreators);
         this.pages = [];
+        this.selectedPage = this.getNewPage();
+
+    }
+    getNewPage(){
+        return {
+            edit: true,
+            _id: 'new',
+            status: 'new',
+            saved: false,
+            title: '',
+            subtitle:'',
+            content: '',
+            description: '',
+            createdOn: new Date()
+        };
     }
     onReceivePages (pages) {
         console.log(pages);
@@ -19,8 +34,23 @@ class PageStore {
         });
     }
     onPageSelected (page) {
+        console.log(page);
         console.log('page');
         this.selectedPage = page;
+    }
+    onComposeSelected() {
+        console.log('onComposeSelected');
+        this.selectedPage = {
+            edit: true,
+            _id: 'new',
+            status: 'new',
+            saved: false,
+            title: 'New Title',
+            subtitle:'SubTitle',
+            content: 'Content Here',
+            description: 'Description',
+            createdOn: new Date()
+        }
     }
 }
 
