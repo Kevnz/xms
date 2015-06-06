2a18470e8fea363ab12f31004da52b26bab46330
Kevin Isom <kevin.isom@gmail.com>
2015-06-06 22:42:13 +1200

    whitespace

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var exposeData = function exposeXMSData (req, res, next) {
 };
 var cms = require('./lib/cms');
 var loader = require('./lib/seed');
+
 exports.handle = cms.handler;
 
 exports.extend = function (app) {
@@ -27,8 +28,7 @@ exports.extend = function (app) {
             };
         })
     });
-
-    //at this stage only one cms content route
+    
     app.use(cms.handler);
     //get admin features and bolt them onto the app
     require('./lib/admin')(app);
